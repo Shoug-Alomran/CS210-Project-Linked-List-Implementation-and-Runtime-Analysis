d5e24dbfb689065841d76cb0fb36d26c3cfb27a1
Shoug Alomran <Shoug.alomran@hotmail.com>
2025-10-10 17:09:25 +0300

    Added methods.

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,5 +1,17 @@
 public class MergeSort implements Sorter {
 
+    @Override
+    public void sort(Registration[] a) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+    }
+
+    @Override
+    public String name() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'name'");
+    }
+
 // Task: Implement sort() using merge sort algorithm
 // Task: Create helper method merge() to combine sorted halves
 // Task: Compare by demandScore (descending order)
