55733bb9137be7cc0abdf16e55b743e6bff4a100
layanbindayel <layanbindayel@gmail.com>
2025-10-10 14:53:37 +0300

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,9 +1,16 @@
 public class Node {
 // Task: Field to store Registration data
+    Registration studData;
 // Task: Field to store next Node reference
+    Node nextRef;
 
 // Task: Constructor to initialize node with Registration
 
-
+    public Node(Registration studData) {
+        this.studData = studData;
+        this.nextRef = null;
+    }
+    
 }
+
 
