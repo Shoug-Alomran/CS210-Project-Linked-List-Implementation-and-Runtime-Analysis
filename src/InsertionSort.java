0613f7e6a1cadcdb52e88cdc70044ccfe495321b
Shoug Alomran <Shoug.alomran@hotmail.com>
2025-10-10 17:11:02 +0300

    Added methods

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,4 +1,16 @@
 public class InsertionSort implements Sorter {
+
+    @Override
+    public void sort(Registration[] a) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+    }
+
+    @Override
+    public String name() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'name'");
+    }
 // Task: Implement sort() using insertion sort algorithm
 // Task: Compare by demandScore (descending order)
 // Task: Return algorithm name in name()
--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,5 +1,17 @@
 public class QuickSort implements Sorter {
 
+    @Override
+    public void sort(Registration[] a) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+    }
+
+    @Override
+    public String name() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'name'");
+    }
+
 // Task: Implement sort() using quick sort algorithm
 // Task: Create helper method partition()
 // Task: Use pivot strategy (middle or random)
