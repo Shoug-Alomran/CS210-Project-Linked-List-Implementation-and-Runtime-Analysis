dca1cfed9e6d92cfa7f32a36e63f9991707ff50a
Shoug Alomran <Shoug.alomran@hotmail.com>
2025-10-10 23:57:34 +0300

    Implemented working code.

--- src/Timer.java
+++ src/Timer.java
@@ -1,10 +1,21 @@
 public class Timer {
-// Task: Field to store start time
-// Task: Field to store end time
+    // Private variables for saftey, no other class can access these directly and effect the elapsed time.
+    private long startTime;
+    private long endTime;
 
-// Task: Method to start timer
-// Task: Method to stop timer
-// Task: Method to get elapsed time in milliseconds
-// Task: (Optional) Method to get elapsed time in nanoseconds
+    // Method to start timer
+    public void start() {
+        startTime = System.currentTimeMillis(); // Simpler than nanoTime
+    }
 
+    // Method to stop timer
+    public void stop() {
+        endTime = System.currentTimeMillis();
+    }
+
+    // Method to get elapsed time in milliseconds
+    public long getElapsedTimeMillis() {
+        return endTime - startTime;
+    }
 }
+
