a32687e048689f13b40ac1ff253bf7408e1b5291
Shoug Alomran <Shoug.alomran@hotmail.com>
2025-10-15 12:17:08 +0300

    Edited a comment.

--- src/FileService.java
+++ src/FileService.java
@@ -4,7 +4,7 @@ public class FileService {
     public static LinkedList readRegistrationsFromFile(String filename) {
         return InputValidator.readAndValidateFile(filename);
     }
-    // Task: Method to write array of registrations to Output.txt
+    // Method to write array of registrations to Output.txt
      public static void writeRegistrationsToFile(Registration[] array, String filename) {
         // TODO: Implement output writing
     }
